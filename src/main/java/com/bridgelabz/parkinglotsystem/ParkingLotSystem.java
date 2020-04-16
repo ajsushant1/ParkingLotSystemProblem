ebc175c22b8050c7c4fa4ddf00721bb27d1d6e65
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-04-16 17:45:02 +0530

    ADDED: Method to unPark vehicle

--- src/main/java/com/bridgelabz/parkinglotsystem/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/parkinglotsystem/ParkingLotSystem.java
@@ -1,11 +1,24 @@
 package com.bridgelabz.parkinglotsystem;
 
 public class ParkingLotSystem {
+    private Object vehicle;
+
     public static void main(String[] args) {
         System.out.println("/**************************/ WELCOME TO PARKING LOT SYSTEM /**************************/");
     }
 
     public boolean park(Object vehicle) {
+        if (this.vehicle != null)
+            return false;
+        this.vehicle = vehicle;
         return true;
+    }
+
+    public boolean unPark(Object vehicle) {
+        if (this.vehicle != null && this.vehicle.equals(vehicle)) {
+            this.vehicle = null;
+            return true;
+        }
+        return false;
     }
 }
