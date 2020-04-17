45a6de3f267893f480b100a230fbfb1e4f5456bb
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-04-17 12:47:13 +0530

    REFACTORED: Methods to thorw parking full and no vehicle exception and ADDED: Methods to check vehicle is park or not

--- src/main/java/com/bridgelabz/parkinglotsystem/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/parkinglotsystem/ParkingLotSystem.java
@@ -7,18 +7,24 @@ public class ParkingLotSystem {
         System.out.println("/**************************/ WELCOME TO PARKING LOT SYSTEM /**************************/");
     }
 
-    public boolean park(Object vehicle) {
+    public void park(Object vehicle) throws ParkingLotSystemException {
         if (this.vehicle != null)
-            return false;
+            throw new ParkingLotSystemException(ParkingLotSystemException.ExceptionType.PARKING_FULL, "Parking is full");
         this.vehicle = vehicle;
-        return true;
     }
 
-    public boolean unPark(Object vehicle) {
-        if (this.vehicle != null && this.vehicle.equals(vehicle)) {
-            this.vehicle = null;
-            return true;
+    public void unPark(Object vehicle) throws ParkingLotSystemException {
+        if (this.vehicle != null && !this.vehicle.equals(vehicle)) {
+            throw new ParkingLotSystemException(ParkingLotSystemException.ExceptionType.NO_VEHICLE, "NO vehicle");
         }
-        return false;
+        this.vehicle = null;
+    }
+
+    public boolean isVehiclePark(Object vehicle) {
+        return this.vehicle.equals(vehicle);
+    }
+
+    public boolean isVehicleUnPark() {
+        return this.vehicle == null;
     }
 }
