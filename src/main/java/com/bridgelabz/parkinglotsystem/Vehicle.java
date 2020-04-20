d4c77bf311b40b75d594e41fa9198e6f809319a2
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-04-20 21:55:15 +0530

    ADDED: Attribute for parking date and time for vehicle

--- src/main/java/com/bridgelabz/parkinglotsystem/Vehicle.java
+++ src/main/java/com/bridgelabz/parkinglotsystem/Vehicle.java
@@ -1,9 +1,21 @@
 package com.bridgelabz.parkinglotsystem;
 
 public class Vehicle {
-    String vehicleId;
+    ParkingDateTime parkingDateTime;
+    private String parkingDateAndTime;
+    private String vehicleId;
 
     public Vehicle(String vehicleId) {
+        parkingDateTime = new ParkingDateTime();
         this.vehicleId = vehicleId;
+        parkingDateAndTime = parkingDateTime.getDateTime();
+    }
+
+    public String getParkingDateAndTime() {
+        return parkingDateAndTime;
+    }
+
+    public String getVehicleId() {
+        return vehicleId;
     }
 }
