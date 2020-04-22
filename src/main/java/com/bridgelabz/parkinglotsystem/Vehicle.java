76a79fc58831d8cfcdab60086fdd56779f162678
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-04-22 15:41:34 +0530

    ADDED: Enum for vehicle size

--- src/main/java/com/bridgelabz/parkinglotsystem/Vehicle.java
+++ src/main/java/com/bridgelabz/parkinglotsystem/Vehicle.java
@@ -3,25 +3,27 @@ package com.bridgelabz.parkinglotsystem;
 public class Vehicle {
     ParkingDateTime parkingDateTime;
     VehicleType vehicleType;
+    VehicleSize vehicleSize;
     private String parkingDateAndTime;
     private String vehicleId;
 
-    public Vehicle(String vehicleId, VehicleType vehicleType) {
+    public Vehicle(String vehicleId, VehicleType vehicleType, VehicleSize vehicleSize) {
         parkingDateTime = new ParkingDateTime();
         this.vehicleId = vehicleId;
         parkingDateAndTime = parkingDateTime.getDateTime();
         this.vehicleType = vehicleType;
+        this.vehicleSize = vehicleSize;
     }
 
     public String getParkingDateAndTime() {
         return parkingDateAndTime;
     }
 
-    public String getVehicleId() {
-        return vehicleId;
-    }
-
     public enum VehicleType {
         NORMAL, HANDICAP
+    }
+
+    public enum VehicleSize {
+        SMALL, LARGE
     }
 }
